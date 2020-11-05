9887111616696c9401f017a0a03ff0f3a5cc29db
Richard.adamo.garcia@gmail.com <Rick@Richards-MacBook-Pro.local>
2020-11-05 14:04:08 -0500

    Finish JS Fundamentals 1 section

--- JavaScript Learning/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript Learning/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -57,4 +57,68 @@ console.log(firstName);
 //     let centruy = 21;
 // }
 
+// const favorite = prompt("What's your favorite number");
 
+//Logical Operators
+
+// const hasDriversLicense = true;
+// const hasGoodVision = false;
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(hasDriversLicense && !hasGoodVision);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision;
+// if (shouldDrive) {
+//     console.log("Sarah is able to drive")
+// }
+// else {
+//     console.log("Someone else should drive");
+// }
+
+//Coding Challenge # 3:
+// const dolphinsAvgScore = (100 + 110 + 110) / 3;
+// const koalasAvgScore = (100 + 110 + 110) / 3;
+
+// if (dolphinsAvgScore > koalasAvgScore) {
+//     console.log("The dolphins are the winners")
+// }
+// else if (dolphinsAvgScore < koalasAvgScore) {
+//     console.log("The Koalas are the winners")
+// }
+// else {
+//     console.log("There was a draw");
+// }
+
+//Switch statement 
+// const day = 'wednesday';
+// switch (day) {
+//     case 'monday':
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//         break;
+//     default:
+//         console.log('Not a valid day!');
+// }
+
+// //The ternary or conditional operator
+// const age = 15;
+// age >= 18 ? console.log('I like to drink wine ') :
+//     console.log("I like to drink water");
+// //Another use
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+
+// console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+// //Coding challenge #4
+// let tip;
+// const bill = 275;
+// tip = bill <= 300 && bill >= 50 ? bill * .15 : bill * .20;
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total was ${bill + tip}`);
