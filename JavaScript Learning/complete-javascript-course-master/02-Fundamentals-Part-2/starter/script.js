6ba8f9bd78b49581cd0f1cbd523f383fee9db309
Richard.adamo.garcia@gmail.com <Rick@Richards-MacBook-Pro.local>
2020-11-05 16:44:27 -0500

    Started fundamentals part 2 JS

---
+++ JavaScript Learning/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,55 @@
+'use strict';
+// //StrictMode
+// let hasDriversLicence = false;
+// let passTest = true;
+
+// if (passTest) hasDriverLicense = true;
+// if (hasDriversLicence) {
+//     console.log('I can drive');
+// }
+
+
+// Functions:
+// function logger() {
+//     console.log('My name is Richard');
+// }
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(5, 3));
+
+//Declarations vs Expressions
+// function calcAge1(birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1999);
+// //This is a function expression
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+// const age2 = calcAge2(1999);
+
+//Arrow Functions:
+//Old function expression:
+// const calcAge2 = function (birthYear) {
+//     return 2037 - birthYear;
+// }
+//arrow version
+// const calcAge3 = birthYear => 2037 - birthYear;
+// console.log(calcAge3);
+// const yearsUntilRetirement = birthYear => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     return retirement;
+// }
+// console.log(yearsUntilRetirement(1999));
+
+//Coding Challenge 1:
+
