4327a88bfc1abe601aebc8cf27fd7c7a0fd11484
Richard.adamo.garcia@gmail.com <Rick@Richards-MacBook-Pro.local>
2020-11-05 12:11:38 -0500

    Completed Up to Section 22 of JS Course

---
+++ JavaScript Learning/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,60 @@
+/*let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Richard";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+*/
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof true);
+// const firstName = "Richard ";
+// const lastName = "Garcia";
+// console.log(firstName + lastName);
+
+
+//Coding Challenge 1:
+
+// const markMass = 95;
+// const markHeight = 1.88;
+// const johnMass = 85;
+// const johnHeight = 1.76;
+// const markBMI = markMass / markHeight ** 2;
+// const JohnBMI = johnMass / johnHeight ** 2;
+// const markHigherBMI = markBMI > JohnBMI;
+// console.log(markHigherBMI);
+
+//Strings and Template literals:
+// const firstName = 'Richard';
+// const job = 'teacher';
+// const birthYear = 1991;
+// const year = 2037;
+// const richard = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old' + job + '!';
+// console.log(richard);
+
+// const richardNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}`;
+// console.log(richardNew);
+// console.log(`String
+// multiple
+// lines`);
+
+//Making Decisions: If Else Statements:
+
+// const age = 19;
+// const isOldEnough = age >= 18;
+// if (isOldEnough) {
+//     console.log('Sara can start driving 😀');
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young, wait another ${yearsLeft} years `);
+// }
+// let century;
+// const birthYear = 1991;
+// if (birthyear <= 2000) {
+//     let century = 20;
+// } else {
+//     let centruy = 21;
+// }
+
+
