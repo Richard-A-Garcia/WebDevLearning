ec16d129c1fd8dce993b2f7eed2be22db63bf665
Richard-A-Garcia <richard.adamo.garcia@gmail.com>
2021-08-30 11:12:14 -0400

    Javascript Section 11

--- AndreiUdemyCourse/Sections 11-20/script.js
+++ AndreiUdemyCourse/Sections 11-20/script.js
@@ -1,26 +1,14 @@
-function sayHello(){
-	console.log("Hello");
-}
-var sayBye = function() { //Anonymous function 
-	console.log("Bye");
-}
-sayBye();
-
-var list = ["tiger", "cat", "bear", "bird"]; //js array 
-
-var user = { //JS object
-	name: "John",
-	age: 34,
-	hobby: "Soccer",
-	isMarried: false,
-};
-
-let todos =[
-	"clean room!",
-	"Brush teeth",
-	"exercise",
-	"study JS"
-];
-for(var i = 0; i< todos.length; i++){
-	console.log(todos.pop());
+//Exercise: Make a Calculator
+// let num1 = prompt("Enter a number");
+// let num2 = prompt("Enter another number");
+// let sum = parseInt(num1) + parseInt(num2);
+// alert(sum);
+//Make a keyless car
+let userAge = parseInt(prompt("What is your age?"));
+if (userAge > 18) {
+	alert("Powering on, enjoy the ride!");
+} else if (userAge === 18) {
+	alert("Congratulations on your first year of driving. Enjoy the ride!");
+} else {
+	alert("Sorry, you are too young to drive this car. Powering off");
 }
