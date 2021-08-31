a20a76c5c31d42b036d3a556499e0641e16623fc
Richard-A-Garcia <richard.adamo.garcia@gmail.com>
2021-08-31 09:43:13 -0400

    Finished JS Section

--- AndreiUdemyCourse/Sections 11-20/script.js
+++ AndreiUdemyCourse/Sections 11-20/script.js
@@ -4,11 +4,18 @@
 // let sum = parseInt(num1) + parseInt(num2);
 // alert(sum);
 //Make a keyless car
-let userAge = parseInt(prompt("What is your age?"));
-if (userAge > 18) {
-	alert("Powering on, enjoy the ride!");
-} else if (userAge === 18) {
-	alert("Congratulations on your first year of driving. Enjoy the ride!");
-} else {
-	alert("Sorry, you are too young to drive this car. Powering off");
-}
+// let userAge = parseInt(prompt("What is your age?"));
+// if (userAge > 18) {
+// 	alert("Powering on, enjoy the ride!");
+// } else if (userAge === 18) {
+// 	alert("Congratulations on your first year of driving. Enjoy the ride!");
+// } else {
+// 	alert("Sorry, you are too young to drive this car. Powering off");
+// }
+
+let user = {
+	name: "John",
+	age: 34,
+	hobby: "Soccer",
+	isMarried: false,
+};
